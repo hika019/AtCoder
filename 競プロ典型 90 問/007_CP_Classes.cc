6028959f547c7de4436346be18553f1068fc0463
hika019 <ikazuti2001@gmail.com>
2021-05-28 19:02:58 +0900

    007

---
+++ 競プロ典型 90 問/007_CP_Classes.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+
+int main(){
+  int N, Q, i;
+  std::cin >> N;
+  int A[N];
+  for(i=0; i<N; i++)std::cin >> A[i];
+  std::cin >> Q;
+  int B[Q];
+  for(i=0; i<Q; i++) std::cin >> B[i];
+
+
+  std::sort(A, A+N);
+
+  for(i=0; i<Q; i++){
+    int index = std::lower_bound(A, A+N, B[i]) - A;
+    int hoge = 2147483647;
+    int fuga = 2147483647;
+
+    if(index <N) hoge = std::abs(B[i]-A[index]);
+
+    if(index >0) fuga = std::abs(B[i]-A[index-1]);
+    std::cout << std::min(hoge, fuga) << '\n';
+
+  }
+
+
+  return 0;
+}
