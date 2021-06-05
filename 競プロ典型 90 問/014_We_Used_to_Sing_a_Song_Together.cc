07e5adf98df7a3bfb4b48a6cb1ec60bc7d6c751d
hika019 <ikazuti2001@gmail.com>
2021-06-05 16:44:27 +0900

    014 AC

---
+++ 競プロ典型 90 問/014_We_Used_to_Sing_a_Song_Together.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+int a[100001];
+int b[100001];
+
+int main(){
+  long long int n, i, ans;
+
+  std::cin >> n;
+
+  for(i=0; i<n; ++i)std::cin >> a[i];
+  for(i=0; i<n; ++i)std::cin >> b[i];
+
+  std::sort(a, a+n);
+  std::sort(b, b+n);
+
+  ans = 0;
+  for(i=0; i<n; ++i){
+    ans += std::abs(a[i]-b[i]);
+  }
+
+  std::cout << ans << '\n';
+
+  return 0;
+}
